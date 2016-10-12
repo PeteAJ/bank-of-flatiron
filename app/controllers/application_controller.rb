e7c174c293d5b60e814bfb07e1e41555625a4486
PeteAJ <peter.jacobson@gmail.com>
2016-10-11 20:24:04 -0400

    basic application controller

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+  set :views, Proc.new { File.join(root, "../views/") }
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "my_secret"
+  end
+
+end
