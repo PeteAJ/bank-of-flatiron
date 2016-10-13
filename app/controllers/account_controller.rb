00cbaf6c1671e5f3258798a73552bc6510b7ca50
PeteAJ <peter.jacobson@gmail.com>
2016-10-13 10:42:01 -0400

    basic accounts restful routes

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,4 +1,22 @@
 class AccountsController < ApplicationController
 
+get '/accounts/new' do #load new acount form
+end
 
+get '/accounts' do #loads index
+end
+
+get '/accounts/:id' do #loads show 1 Account
+end
+
+get 'accounts/:id/edit' do #loads edit form\
+end
+
+patch '/accounts/:id' do #updates accounts
+end
+
+post '/accounts' do #creates an AccountsControllere
+end
+
+delete '/accounts/:id/delete' do #deletes account - exclude?
 end
