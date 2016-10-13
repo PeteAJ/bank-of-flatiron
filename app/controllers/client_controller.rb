78e615d5f320206a3e8efcdb5e831d36dfe7e066
PeteAJ <peter.jacobson@gmail.com>
2016-10-13 10:55:11 -0400

    basic clients restful routes

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,4 +1,19 @@
 class ClientsController < ApplicationController
 
+get '/clients/new' do #loads new client form
+end
 
+get '/clients' do #loads index
+end
+
+get '/clients/:id' do #show 1 client
+end
+
+get '/clients/:id/edit' do #edit client form
+end
+
+patch '/clients/:id/' do #updates clients
+end
+
+post '/clients' do #creates a client
 end
