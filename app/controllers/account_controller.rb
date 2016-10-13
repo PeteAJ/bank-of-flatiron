8d46e3ff8bf5c959794e5f8822cb1012b03a52dd
PeteAJ <peter.jacobson@gmail.com>
2016-10-13 11:20:48 -0400

    full accounts controller

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,22 +1,40 @@
 class AccountsController < ApplicationController
 
 get '/accounts/new' do #load new acount form
+  erb :'/accounts/new'
 end
 
 get '/accounts' do #loads index
+  @accounts = Account.all
+  erb :'/accounts/index'
 end
 
 get '/accounts/:id' do #loads show 1 Account
+  @account = Account.find_by_id(params[:id])
+  erb ':/accounts/show'
 end
 
-get 'accounts/:id/edit' do #loads edit form\
+get 'accounts/:id/edit' do #loads edit form
+  @account = Account.find_by_id(params[:id])
+  erb ':/accounts/edit'
 end
 
 patch '/accounts/:id' do #updates accounts
+  @account = Account.find_by_id(params[:id])
+  @account.name = params[:name]
+  @account.balance = params[:balance]
+  @account.overdraft_protection = params[:overdraft_protection]
+  @account.save
+  redirect to '/accounts/#{@account.id}'
 end
 
-post '/accounts' do #creates an AccountsControllere
+post '/accounts' do #creates an Account
+  @account = Account.create(params)
+  redirect to '/accounts/#{@account.id}'
 end
 
 delete '/accounts/:id/delete' do #deletes account - exclude?
+  @account = Account.find_by_id(params[:id])
+  @account.delete
+  redirect to '/accounts'
 end
