d4ad3e807ee2a0c4416c45796b652dfaa5703210
PeteAJ <peter.jacobson@gmail.com>
2016-11-06 21:28:23 -0500

    link for second acct menu clients/show

--- app/models/account.rb
+++ app/models/account.rb
@@ -11,13 +11,22 @@ class Account < ActiveRecord::Base
     self.accounts.create(name: from, name: to, amount: amount)
   end
 
-  def valid_transaction?(name, amount)
-    self.transactions.create(name: name, amount: amount)
-      #if no overdraft_protection, withdrawl ok to zero
-      new_balance = account.balance - params[:transaction_amount].to_i
-      if !account.overdraft_protection && new_balance < 0
-        flash[:notice] = "*rejected - insufficient funds!*"
-      end
-        redirect to "/accounts/#{account.id}"
+  def valid_transaction()
+
+    if !account.overdraft_protection && new_balance < 0
+              flash[:notice] = "Withdrawl rejected - insufficient funds!"
+             redirect to "/accounts/#{account.id}"
+            end
+
+
+  if !origin_account.overdraft_protection && new_balance < 0
+          flash[:notice] = "Withdrawl rejected - insufficient funds!"
+          redirect to "/accounts"
+        end
+
+
   end
+
+
+
 end
